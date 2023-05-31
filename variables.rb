e907053df21ecefb5b7f25753df017cc69457f85
Antoine Aube <aaube3@gmail.com>
2023-05-31 12:36:09 -0600

    add integer variables

--- variables.rb
+++ variables.rb
@@ -3,3 +3,10 @@ first_name = "Antoine"
 last_name = "Aube"
 favorite_food = "Falafel"
 favorite_activity = "Climbing"
+
+#integer variables
+age = 31
+year_of_birth = 1991
+number_of_siblings = 1
+number_of_monitors = 2
+
